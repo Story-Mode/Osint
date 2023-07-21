0773f0fac231b0d70b13ca4ab3f33002f59e892b
Story-Mode <138918216+Story-Mode@users.noreply.github.com>
2023-07-21 14:14:29 -0500

    Update Script5.sh

--- Script5.sh
+++ Script5.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-input =“./names.txt”   
+input=“./names.txt”  # This line directs the following to be places in the text file 
 while IFS=, read -r first last;      # notice the comma after the equal sign 
   do
-name="$first $last"
-username="${first:0:1}$last”
- n=`egrep –c $username /etc/passwd`
+name="$first $last" # this states the user name by first and last name
+username="${first:0:1}$last” 
+ n=`egrep –c $username /etc/passwd` 
  n=$((n+1))
  username=$username$n
 useradd -m $username -s /bin/bash
