853fad1850e62a056dc81b384d4272122eed244e
Story-Mode <138918216+Story-Mode@users.noreply.github.com>
2023-07-21 13:45:50 -0500

    Update Script3.sh

--- Script3.sh
+++ Script3.sh
@@ -2,6 +2,6 @@
 x=10               #NOT x   =  10 no spaces
 X=20               #variables are case sensitive
 y=                    #NULL variable
-echo "x = $x"
-echo "X = $X"
-echo "y = $y"
+echo "x = $x" # this is repeating the value of 'x' from above
+echo "X = $X" # this is repeating the value of 'X' from above
+echo "y = $y" # this is repeating the value of 'y' which is NULL currently
