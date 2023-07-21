cad8e11538c842eb98c31c28b0f279e5404d1d57
Story-Mode <138918216+Story-Mode@users.noreply.github.com>
2023-07-21 14:05:36 -0500

    Update Script4.sh

--- Script4.sh
+++ Script4.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-input =“./names.txt”   
+input=“./names.txt”  # this creates a text files  
 while IFS=, read -r first last; # notice the comma after the equal sign
- do
-name="$first $last"
-username="${first:0:1}$last"
-useradd -m $username -s /bin/bash
+ do # This initializes 
+name="$first $last" # This showes users by first name and then last name
+username="${first:0:1}$last" # This presents the usernames with their first initial and last name
+useradd -m $username -s /bin/bash # this adds a user to
  done < "$input"
 tail -8 /etc/passwd
